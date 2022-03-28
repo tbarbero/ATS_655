ddda61f76825b03f5618845d728a05e565023408
Tyler Barbero <tbarbero@ucsd.edu>
2022-03-28 16:57:48 -0600

    adding repo again

---
+++ HW2/fig_params.py
@@ -0,0 +1,14 @@
+def fig_params():
+    import matplotlib.pyplot as plt
+    
+    large = 28; med = 24; small = 15
+    params = {'axes.titlesize': small,
+          'legend.fontsize': small,
+          'figure.figsize': (8, 4),
+          'axes.labelsize': small,
+          'axes.titlesize': small,
+          'xtick.labelsize': small,
+          'ytick.labelsize': small,
+          'figure.titlesize': med,
+          'axes.titlepad': 10}
+    plt.rcParams.update(params)
